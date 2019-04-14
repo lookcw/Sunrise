bc2f72487161e98d5fcce2854800213392b7de56
lookcw <lookwchristopher@gmail.com>
2019-04-14 08:07:53 -0400

    finalized chrome extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,6 +1,5 @@
 let changeColor = document.getElementById('changeColor');
 let area = document.getElementsByTagName("input");
-console.log(area)
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
@@ -8,9 +7,9 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
     let color = element.target.value;
-    console.log("sajfaiwoefhlawehfifhlaiefhawiefhwaei");
-    let area = document.getElementsByTagName("input");
-console.log(area);
+    // let area = document.getElementsByTagName("input");
+    let area = document ;
+    console.log("Monitoring on");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
